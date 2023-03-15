0dc7923b5dad71d0125435901a82c3f8ab6293b1
Antonov333 <antonovsergei333@yahoo.com>
2023-03-15 17:21:29 +0300

    Initial commit for Homework11

---
+++ src/Homework11.java
@@ -0,0 +1,29 @@
+public class Homework11 {
+
+    static int hw = 11 ;
+
+    public static void main(String[] args) {
+        printHwTitle();
+        task_();
+        printCompletionMessages();
+
+    }
+
+    public static void printHwTitle() {
+        System.out.println("ДОМАШНЕЕ ЗАДАНИЕ №" + hw);
+        System.out.println(" ");
+    }
+
+    public static void task_() {
+        System.out.println("Problem #_");
+        System.out.println("Solution to be provided soon");
+        System.out.println("");
+    }
+
+
+    public static void printCompletionMessages() {
+        String hwStatus = " is in progress" ;
+        System.out.println("Домашнее задание " + hw + hwStatus);
+        System.out.println(" ");
+    }
+}
